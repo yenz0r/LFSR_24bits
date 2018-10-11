341c501f6bf56913ad73fca138ca57bd6b1f5fdc
yenz0redd <375293774737@yandex.ru>
2018-10-11 19:31:33 +0300

    final

---
+++ LFSR_stream_cipher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LFSR_stream_cipher
+//
+//  Created by Egor Pii on 10/6/18.
+//  Copyright © 2018 yenz0redd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LFSR_stream_cipher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LFSR_stream_cipher
+//
+//  Created by Egor Pii on 10/6/18.
+//  Copyright © 2018 yenz0redd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
